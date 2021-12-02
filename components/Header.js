55a054ac9bed0cef7586d56110b3dabdacd1846d
Joseph Mangubat <josephmangubat@josephs-mini.mynet>
2021-12-02 21:58:41 +0000

    pyNotes

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+
+      {/* Left */}
+      <div></div>
+      {/* Center */}
+      {/* Right */}
+    </div>
+  );
+}
+
+export default Header;

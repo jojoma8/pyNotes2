f90c30c809043cc8acae65d67cd73e201ffdb202
jojoma8 <jojo_ma@hotmail.co.uk>
2021-12-05 01:07:34 +0000

    removeClient

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,8 +10,8 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
     // ...add more providers here
   ],

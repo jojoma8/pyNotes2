9e0aad42db160f20a7e742da740266e0b21c7d90
jojoma8 <jojo_ma@hotmail.co.uk>
2021-12-05 10:09:43 +0000

    fixAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 // import GithubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from `next-auth/providers/google`;
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,8 +11,12 @@ export default NextAuth({
     //   clientSecret: process.env.GITHUB_SECRET,
     // }),
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     // ...add more providers here
   ],

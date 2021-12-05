4967c2b13f226dab2c46c3f59d9d49368f1befe3
jojoma8 <jojo_ma@hotmail.co.uk>
2021-12-05 10:14:36 +0000

    urlUpdate

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ module.exports = {
   images: {
     domains: ["1000logos.net", "platform-lookaside.fbsbx.com"],
   },
-  env: (NEXTAUTH_URL = "https://py-notes2.vercel.app/"),
+  env: (NEXTAUTH_URL = "https://py-notes2-er7yq8hxk-jojoma8.vercel.app/"),
 };
